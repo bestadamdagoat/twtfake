787e8f2dad325e460b6e81dc57ddb4780c7bc654
Adam O <66372881+bestadamdagoat@users.noreply.github.com>
2023-12-14 19:51:18 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,20 +6,23 @@ async function handleRequest(request) {
   const { searchParams } = new URL(request.url);
   const fakelink = searchParams.get('fakelink');
   const reallink = searchParams.get('reallink');
+  const useragent = searchParams.get('useragent');
 
   if (fakelink && reallink) {
-    if (request.headers.get('User-Agent').includes('Twitterbot')) {
-      // If the user agent is Twitterbot, redirect to fakelink
-      return Response.redirect(fakelink, 301);
-    } else {
-      // Redirect to reallink for other user agents
-      return Response.redirect(reallink, 301);
+    if (useragent) {
+      // Check if the specified user-agent matches the request's user-agent
+      if (request.headers.get('User-Agent') === useragent) {
+        return Response.redirect(fakelink, 301);
+      }
     }
+
+    // If user-agent is not specified or doesn't match, redirect to reallink
+    return Response.redirect(reallink, 301);
   } else {
     // If no params are provided, redirect to specified page
-    return Response.redirect('https://github.com/bestadamdagoat/twtfake', 301);
+    return Response.redirect('https://github.com/bestadamdagoat/uatools', 301);
   }
 }
-// https://url.com/?fakelink=https://example.com/fake&reallink=https://example.com/real
+// https://url.com/?fakelink=https://example.com/fake&reallink=https://example.com/real&useragent=Twitterbot
 // shows example.com/fake on twitter
 // shows example.com/real after clicking
